cc7b985c267b0c50cf028d5342c7b037089b4495
lcw1314666666 <1820566696@qq.com>
2020-11-26 23:30:43 +0800

    add tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
+    <TarBar>
+        <TarBarItem path='/home' activeColor='red'>
+          <template v-slot:item-icon>
+            <img src="./assets/tabbarImg/img/shouye.svg" alt="">
+          </template>
+          <template v-slot:item-icon-active>
+            <img src="./assets/tabbarImg/img/shouyeActive.svg" alt="">
+          </template>
+          <template v-slot:item-text>
+            <div>首页</div>
+          </template>   
+        </TarBarItem>
+        <TarBarItem path="/cart" activeColor='red'>
+          <template v-slot:item-icon>
+            <img src="./assets/tabbarImg/img/gouwuche.svg" alt="">
+          </template>
+          <template v-slot:item-icon-active>
+            <img src="./assets/tabbarImg/img/gouwucheActive.svg" alt="">
+          </template>
+          <template v-slot:item-text>
+            <div>购物车</div>
+          </template> 
+        </TarBarItem>
+        <TarBarItem path="category" activeColor="red">
+          <template v-slot:item-icon>
+            <img src="./assets/tabbarImg/img/fenlei.svg" alt="">
+          </template>
+          <template v-slot:item-icon-active>
+            <img src="./assets/tabbarImg/img/fenleiActive.svg" alt="">
+          </template>
+          <template v-slot:item-text>
+            <div >分类</div>
+          </template> 
+        </TarBarItem>
+        <TarBarItem path="profile" activeColor="red">
+          <template v-slot:item-icon>
+            <img src="./assets/tabbarImg/img/wode.svg" alt="">
+          </template>
+          <template v-slot:item-icon-active>
+            <img src="./assets/tabbarImg/img/wodeActive.svg" alt="">
+          </template>
+          <template v-slot:item-text>
+            <div>我的</div>
+          </template> 
+        </TarBarItem>
+      </TarBar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TarBar from './components/tabbar/tabbar'
+import TarBarItem from './components/tabbar/tabbarItem'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TarBar,
+    TarBarItem
   }
 }
 </script>
 
 <style>
+@import url('./assets/css/base.css');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +72,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img{
+  width: 24px;
+  height: 24px;
+  margin-top: 6px;
+}
+div{
+  height: 24px;
+  line-height: 24px;
 }
 </style>
---
+++ src/router/router.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Home = () => import('../views/home/home.vue')
+const Cart = () => import('../views/cart/cart.vue')
+const Category = () => import('../views/category/category.vue')
+const Profile = () => import('../views/profile/profile.vue')
+
+
+const routes = [
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Cart
+  },
+  {
+    path: '/category',
+    name: 'Category',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Category
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Profile
+  },
+
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+export default router
